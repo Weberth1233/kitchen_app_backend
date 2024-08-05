a331dc83da269ecce85c4bd74a0fe0911314fa4e
Weberth1233 <weberth20-19@hotmail.com>
2024-08-05 17:23:52 -0300

    fix

--- kitchen_app/urls.py
+++ kitchen_app/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
     #Food urls
     path("foods/", views.fetch_foods, name="foods"),
     #------------------------------------------------
--- kitchen_app/views.py
+++ kitchen_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from rest_framework.decorators import api_view, permission_classes
 from kitchen_app.models import Food, Category, Recipe, Step
 from kitchen_app.serializers import FoodSerializer, CategorySerializer, RecipeSerializer, StepSerializer
@@ -7,9 +6,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
 # Create your views here.
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
 
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
@@ -57,7 +53,6 @@ def get_recipe_by_category(request, pk):
         serializer = RecipeSerializer(recipes, many = True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     return Response({"error_request": "Requisição incorreta - Use o método GET."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
 
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
